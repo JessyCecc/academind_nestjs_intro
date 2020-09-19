c0aef251fe4c5b6c1b34571c0d3ec653ac231628
dev-jc <jean-christophe.ceccaldi@oxatis.com>
2020-09-19 13:49:00 +0200

    no message

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,20 +10,12 @@ export class ProductsService {
 
     constructor(@InjectModel('Product') private readonly productModel: Model<Product>) {}
 
-    async insertProduct(title: string,
-                  desc: string,
-                  price: number
-    ) {
-        // const prodId = Math.random().toString();
-        // const prodId = (this.products.length + 1).toString();
-        // const prodId = new Date().toString();
+    async insertProduct(title: string, desc: string, price: number) {
         const newProduct = new this.productModel({
             title,
             description: desc,
             price,
         });
-        // this.products.push(newProduct);
-        // return prodId;
         const result = await newProduct.save();    // await will wait for the promise newProduct.save to be complete (we have to add async to the function)
         // console.log('ProductsService.insertProduct save result', result);
         return result.id as string;
@@ -35,51 +27,57 @@ export class ProductsService {
         return products as Product[];       // we can be clear by adding "as Product[]" of which type
     }
 
-    getSingleProduct(productId: string) {
-        const [product] = this.findProduct(productId);
+    async getSingleProduct(productId: string) {
+        const product = await this.findProduct(productId);
         if (!product) {
             throw new NotFoundException('Could not found product.');
         }
-        return {...product};
+        return product;
     }
 
-    updateProduct(productId: string,
+    async updateProduct(productId: string,
                   title: string,
                   desc: string,
                   price: number
     ){
-
-        const [product, productIndex] = this.findProduct(productId);
-        // we would like to modify only data that are specify in the body and not the undify data
-        const updatedProduct = {...product};
-        let updated = false;
-        if (title)  { updatedProduct.title = title; updated=true; }
-        if (desc)   { updatedProduct.description = desc; updated=true; }
-        if (price)  { updatedProduct.price = price; updated=true; }
-        if (!updated) {
-            throw new NotFoundException('No data to modify');
-        }
-        this.products[productIndex] = updatedProduct;
+        // const updatedProduct = await this.findProduct(productId);
+        // // we would like to modify only data that are specify in the body and not the undify data
+        // let updated = false;
+        // if (title)  { updatedProduct.title = title; updated=true; }
+        // if (desc)   { updatedProduct.description = desc; updated=true; }
+        // if (price)  { updatedProduct.price = price; updated=true; }
+        // if (!updated) {
+        //     throw new NotFoundException('No data to modify');
+        // }
+        // this.products[productIndex] = updatedProduct;
         return {result: 1};
     }
 
     deleteProduct(productId: string) {
-        const [_, index] = this.findProduct(productId);   //alternative
-        // const index = this.findProduct(productId)[1];
-        this.products.splice(index, 1);
+        // const [_, index] = this.findProduct(productId);   //alternative
+        // // const index = this.findProduct(productId)[1];
+        // this.products.splice(index, 1);
         return {result: 1};
     }
 
     // ----- --------------------------------------------------
 
-    private findProduct(id: string): [Product, number] {
-        // const product = this.products.find((prod) => prod.id === id);
-        const productIndex = this.products.findIndex((prod) => prod.id === id);
-        const product = this.products[productIndex];
-        if (!product) {
-            throw new NotFoundException('Could not found product.');
+    private async findProduct(id: string): Promise<Product> {
+        let product = null;
+        try {
+            product = await this.productModel.findById(id);
+        } catch(error) {
+            throw new NotFoundException('This id is invalid ('+id+').');
         }
-        return [product, productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not found product with this id '+id+'.');
+        }
+        return {
+            id: product.id,
+            title: product.title,
+            description: product.description,
+            price: product.price,
+        };
     }
 
 }
